f4feb1818bebf617424c24293b08c9776bb076b1
奥野周 <hiroshi@okunoamanenoMacBook-Air.local>
2023-07-12 21:17:01 +0900

    <NODE_ENV>

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
-
+const NODE_ENV = "production";
 ReactDOM.render(
   <React.StrictMode>
     <App />
