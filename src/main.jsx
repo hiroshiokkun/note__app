6ccfda4201c3ceb430302312edbae3ef8949ecd4
奥野周 <hiroshi@okunoamanenoMacBook-Air.local>
2023-07-21 21:17:04 +0900

    <NODE_ENV>

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
+
+// NODE_ENV環境変数の設定（任意）
 const NODE_ENV = "development";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
-export default main;
+
+export default NODE_ENV;
